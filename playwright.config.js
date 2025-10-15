24b8ee1d9345362e5e00c5170cd1ea06f2bf00fd
malekshoayb-ace <malekshoayb.ace@gmail.com>
2025-10-15 14:40:56 +0530

    Note

--- playwright.config.js
+++ playwright.config.js
@@ -48,11 +48,11 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+   /* {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
